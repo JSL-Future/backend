a63b713c79a54df0eeb212a94f9d16743aa2ed34
Alexandre Soares <ale_santos.soares@hotmail.com>
2020-12-02 10:18:20 -0300

    controllers: add new route

--- routes/user/index.js
+++ routes/user/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const { userController } = require('../../controllers')
 
+router.get('/users', userController.getAll)
 router.post('/users', userController.create)
 router.put('/users/:id', userController.update)
 router.get('/users/:id', userController.getById)
