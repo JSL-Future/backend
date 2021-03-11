b7a69c6fd778aecc52710e56347c0aee193dd7f6
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-03-11 08:24:30 -0300

    add logs

--- controllers/authentication/index.js
+++ controllers/authentication/index.js
@@ -6,18 +6,24 @@ const UserModel = database.model('user')
 const secret = process.env.SECRET
 
 const authentication = async (req, res, next) => {
+ console.log('primeiro 1')
  try {
+  console.log('primeiro 2')
   const user = await UserModel.findOne({ where: { document: req.body.document } })
+  console.log('primeiro 3')
   const checkedPassword = await compare(req.body.password, user.password)
-
-  if(!checkedPassword) {
+  console.log('primeiro 4')
+  if(!checkedPassword) {  
+    console.log('primeiro 5')
     throw new Error('Username or password do not match')
   }
-
+  console.log('primeiro 6')
   const token = jwt.sign({ user }, secret, { expiresIn: '24h'})
+  console.log('primeiro 7')
   res.json({ user, token })
-
+  console.log('primeiro 8')
  } catch (error) {
+  console.log('primeiro 9', error)
    res.status(400).json({ errors: [{ error: error.name, message: error.message }]})
  }
 
