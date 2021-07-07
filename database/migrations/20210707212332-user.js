56190ef2314cefbfe2c1617a6aa61caa6fca289e
Alexandre Soares <ale_santos.soares@hotmail.com>
2021-07-07 18:38:45 -0300

    chore: create migration

---
+++ database/migrations/20210707212332-user.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('users', {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    document: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    companyId: {
+      type: Sequelize.UUID,
+      references: {
+        model: 'companies',
+        key: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'restrict',
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+  down: (queryInterface) => queryInterface.dropTable('users')
+};
---
+++ database/migrations/20210707212422-maintenanceOrder.js
@@ -0,0 +1,122 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('maintenanceOrders', {
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    activated: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    },
+    driverMain:{
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    driverPhoneMain:{
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    driverMainLicense: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    driverSecondaryLicense: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    driverPhoneSecondary:{
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    driverSecondary:{
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    plateHorser: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    plateCart: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    maintenanceDate: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    costCenter: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    priority: {
+      type: Sequelize.ENUM(['low', 'medium', 'high']),
+      allowNull: false,
+      defaultValue: 'low',
+    },
+    service: {
+      type: Sequelize.ENUM(['preventive', 'corrective']),
+      allowNull: false,
+      defaultValue: 'preventive',
+    },
+    serviceDescription: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: Sequelize.ENUM([
+        'solicitation',
+        'check-in',
+        'avaiable',
+        'parking',
+        'courtyard',
+        'awaiting_repair',
+        'dock',
+        'wash',
+        'supply',
+        'check-out',
+      ]),
+      allowNull: false,
+      defaultValue: 'solicitation',
+    },
+    operationId: {
+      type: Sequelize.UUID,
+      references: {
+        model: 'operations',
+        key: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'restrict',
+    },
+    userId: {
+      type: Sequelize.UUID,
+      references: {
+        model: 'users',
+        key: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'restrict',
+    },
+    companyId: {
+      type: Sequelize.UUID,
+      references: {
+        model: 'companies',
+        key: 'id',
+      },
+      onUpdate: 'cascade',
+      onDelete: 'restrict',
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+  down: (queryInterface) => queryInterface.dropTable('maintenanceOrders')
+};
